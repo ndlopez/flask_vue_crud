29a91f68dd4d41c4f9b504ce0bc18892f4a26e5b
Diego Lopez <ndzerglink@live.jp>
2022-09-12 12:55:04 +0900

    Modified to fix, couldnt

--- README.md
+++ README.md
@@ -2,12 +2,18 @@
 
 This app is based on the following [tutorial](https://testdriven.io/blog/developing-a-single-page-app-with-flask-and-vuejs/).
 
+List installed NPM packages:<br>
+    $ npm list -g --depth=0
+    
 ## Running Environment
 - OS : Windows 11 Pro
 - Vue: 
+    * cli-service-global v4.5.17
+    * cli v5.0.8
+- axios: v0.26.1
+- bootstrap v5.1.3
 - Node: v16.14.2
 - npm: v8.5.0
 - Python: v3.10
 - Flask: v2.1.1
 - Flask CORS: v3.0.10
-
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,24 +1,25 @@
 /* This file is the router */
 /* eslint-disable */
-import Vue from 'vue';
+import * as Vue from 'vue';
 import Router from 'vue-router';
 import BookThis from '../components/BookThis.vue';
 import PingMe from '../components/PingMe.vue';
 
 Vue.use(Router);
 
+let routes = [
+  {
+    path: '/',
+    name: 'Books',
+    component: BookThis,
+  },
+  {
+    path: '/ping',
+    name:'PingMe',
+    component:PingMe,},
+]
 export default new Router({
 	mode: 'history',
   base: process.env.BASE_URL,
-  routes:[
-    {
-      path: '/',
-      name: 'Books',
-      component: BookThis,
-    },
-    {
-      path: '/ping',
-      name:'PingMe',
-      component:PingMe,},
-  ],
+  routes
 });
