71effe4601b61b816a656d1f2ff836dbfedfd733
DLopez <26180179+ndlopez@users.noreply.github.com>
2023-09-28 22:00:44 +0900

    added methods to update gui without reload

--- client2/get_books.js
+++ client2/get_books.js
@@ -13,7 +13,7 @@ let thisId= "";
     let txt = "<tr><th>Author</th><th>Title</th><th>Read?</th><th></th></tr>";
 
     for (let idx=0;idx < gotBooks.length; idx++){
-        txt += `<tr><td>${gotBooks[idx]['author']}</td><td>${gotBooks[idx]['title']}</td><td>${gotBooks[idx]['read']}</td><td><button onclick="edit_book('${gotBooks[idx]['id']}')">Update</button><button class="delete" onclick="del_book('${gotBooks[idx]['id']}')">Delete</button></td></tr>`;
+        txt += `<tr><td>${gotBooks[idx]['author']}</td><td>${gotBooks[idx]['title']}</td><td>${gotBooks[idx]['read']}</td><td><button class="update" onclick="edit_book('${gotBooks[idx]['id']}')">Update</button><button class="delete" onclick="del_book('${gotBooks[idx]['id']}')">Delete</button></td></tr>`;
     }
 
     tab.innerHTML = txt;
@@ -46,7 +46,7 @@ function add_book(this_author,this_title,this_bool){
     .then((json) => console.log(json));
     /* Update client-side */
     const trEl = document.createElement("TR");
-    trEl.innerHTML = `<td>${postData['author']}</td><td>${postData['title']}</td><td>${postData['read']}</td><td><button>Update</button onclick="edit_book()"><button class="delete" onclick="del_book()">Delete</button></td>`;
+    trEl.innerHTML = `<td>${postData['author']}</td><td>${postData['title']}</td><td>${postData['read']}</td><td><button>Update</button class="update" onclick="edit_book()"><button class="delete" onclick="del_book()">Delete</button></td>`;
 
     document.getElementsByTagName("tbody")[0].appendChild(trEl);
     //console.log("new item",tab,trEl);
@@ -60,35 +60,16 @@ function del_book(bookId){
         body: '',
         headers: { "Content-Type": "application/json; charset=UTF-8"}
     });
-    /* Update gui by hiding bookiD parent*/
+    /* Update gui by removing bookiD parent*/
     const delBtn = document.getElementsByClassName("delete");
     for (let idx=0;idx < delBtn.length;idx++){
         if (delBtn[idx].outerHTML.includes(bookId)){
             console.log("Bye bye",delBtn[idx].outerHTML);
             const task = delBtn[idx].parentElement.parentElement;
-            task.style.display = "none";
+            task.remove();
+            // task.style.display = "none";
         }
     }
-}
-
-async function remove_item(){
-    /*How to remove item without id*/
-    const delBtn = document.getElementsByClassName("delete");
-    let jdx = 0;
-    const data = await get_books();
-    for (let idx= 0;idx<delBtn.length;idx++){
-        delBtn[idx].onclick = function(){
-            const task = this.parentElement.parentElement;
-            task.style.display = "none";
-            for (let kdx = 0; kdx < data.length; kdx++) {
-                if(data[kdx]['author'] == task.childNodes[0].innerHTML){
-                    jdx = kdx;
-                }
-            }
-            console.log("Bye bye ",task.childNodes[0].innerHTML);
-        }
-    }
-    del_book(data[jdx]['id']);
 }
 
 async function edit_book(bookId){
@@ -129,19 +110,35 @@ function putData(){
     .then((response)=>response.json())
     .then((json)=>console.log(json));
     closeNav()
+    // Updating the GUI without reloading the page
+    const editItem = document.getElementsByClassName("update");
+
+    for(let idx = 0;idx < editItem.length; idx++){
+        if (editItem[idx].outerHTML.includes(bookId)){
+            console.log(bookId,"Edited");
+            const thisAuth = editItem[idx].parentElement.parentElement;
+            thisAuth.innerHTML = `<td>${putData.author}</td><td>${putData.title}</td><td>${putData.read}</td><td><button class="update" onclick="edit_book()">Update</button><button class="delete" onclick="del_book()">Delete</button></td>`;
+        }
+    }
 }
 
-function add_form(){
+function add_form(thisTitle="Add a new book",edit=false){
     const formDiv = document.createElement("section");
     formDiv.id = "addBook";
     formDiv.classList.add("modal");
     //formDiv.innerHTML = "<h3>Add a new book</h3>";
     //let txt = "";
-    formDiv.innerHTML = "<div class='modal-content'><div><h3>Add a new book</h3><span class='close' onclick=closeNav()>&times;</span></div><div><form><label for='fauthor'>Author</label><br><input type='text' id='fauthor' name='fauthor'><br>" +
-    "<label for='ftitle'>Title</label><br><input type='text' id='ftitle' name='ftitle'><br><input type='checkbox' id='book_read' name='book_read' value='Read?'><label for='book_read'>Read?</label><br><input type='button' value='Submit' onclick='get_form()'><input type='button' value='Edit' onclick='putData()'></form></div></div>";
+    let buttons = "<input type='button' value='Submit' onclick='get_form()'><input type='button' value='Edit' onclick='putData()' disabled>";
+    if (edit){
+        buttons = "<input type='button' value='Submit' onclick='get_form()' disabled><input type='button' value='Edit' onclick='putData()'>";
+    }
+    formDiv.innerHTML = "<div class='modal-content'><div><h3>"+ thisTitle + "</h3><span class='close' onclick=closeNav()>&times;</span></div><div><form><label for='fauthor'>Author</label><br><input type='text' id='fauthor' name='fauthor'><br><br>" +
+
+    "<label for='ftitle'>Title</label><br><input type='text' id='ftitle' name='ftitle'><br><br><input type='checkbox' id='book_read' name='book_read' value='Read?'><label for='book_read'>Read?</label><br><br>" + buttons + "</form></div></div>";
     window.onclick = function(ev){
         if (ev.target == formDiv){
-            formDiv.style.display = "none";
+            formDiv.remove();
+            // formDiv.style.display = "none";
         }
     }
     return formDiv;
@@ -157,10 +154,35 @@ function get_form(){
 }
 
 function closeNav(){
-    document.getElementById('addBook').style.display = "none";    
+    /*
+    openNav creates a new elem (section) every time, by setting
+    document.getElementById('addBook').style.display = "none";
+    it hides the elem and cannot modify it*/
+    document.getElementById('addBook').remove();
 }
 
 function openNav(){
+    mainDiv.appendChild(add_form());
     const thisDiv = document.getElementById('addBook');    
     thisDiv.style.display = "block";
+}
+
+async function remove_item(){
+    /*How to remove item without id. Currently not working*/
+    const delBtn = document.getElementsByClassName("delete");
+    let jdx = 0;
+    const data = await get_books();
+    for (let idx= 0;idx<delBtn.length;idx++){
+        delBtn[idx].onclick = function(){
+            const task = this.parentElement.parentElement;
+            task.style.display = "none";
+            for (let kdx = 0; kdx < data.length; kdx++) {
+                if(data[kdx]['author'] == task.childNodes[0].innerHTML){
+                    jdx = kdx;
+                }
+            }
+            console.log("Bye bye ",task.childNodes[0].innerHTML);
+        }
+    }
+    del_book(data[jdx]['id']);
 }
