0f4647e99219ebd16f8a6c6cfc60810914ef1b08
DLopez <26180179+ndlopez@users.noreply.github.com>
2023-09-28 21:47:45 +0900

    Added func/route to get one item from server

--- server/app.py
+++ server/app.py
@@ -9,7 +9,7 @@ import uuid
 from urllib import request
 from flask import Flask,jsonify, request
 from flask_cors import CORS
-
+import json
 #config
 DEBUG=True
 
@@ -20,7 +20,14 @@ app.config.from_object(__name__)
 #enable CORs
 CORS(app, resources={r'/*':{'origins':'*'}})
 
-BOOKS = [
+def read_json():
+    # open a file on server side
+    with open(r"/Users/yourstruly/Documents/flask_vue_crud/server/books.json","r") as fp:
+        book_data = json.load(fp)
+    return book_data["bock"]
+
+BOOKS=read_json()
+"""BOOKS = [
     {
         'id':uuid.uuid4().hex,'title':'On the Verge','author':'Candice Dare','read':False
     },{
@@ -28,7 +35,7 @@ BOOKS = [
     },{
         'id':uuid.uuid4().hex,'title':'Server side and its applications','author':'Rose Monroe','read':True
     },
-    ]
+    ]"""
 
 def remove_book(book_id):
     for book in BOOKS:
@@ -80,6 +87,24 @@ def single_book(book_id):
         remove_book(book_id)
         response_obj['message'] = 'Book removed'
     return jsonify(response_obj)
+
+@app.route('/books/author=<author>',methods=['GET'])
+def list_item(author):
+    response_obj = {'status':'success'}
+    # search for author in BOOKS dic?
+    aux = next(item for item in BOOKS if author in item["author"])
+
+    # my attempt
+    """for item in BOOKS:
+        if author in item["author"]:
+            return jsonify(item)"""
+    # get the index
+    # idx = ((i for i,item in enumerate(BOOKS) if author in item["name"]),None)
+    # return jsonify(BOOKS[idx])
+    # using filter func
+    # aux = list(filter(lambda person: author in person["author"], BOOKS))
+    return jsonify(aux)
+    # return jsonify(response_obj)
 
 if __name__ == "__main__":
     app.run(port=3872)
